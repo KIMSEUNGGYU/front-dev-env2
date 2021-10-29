3faeff7bf11b1da472bd5dd2e4d8798281ee789d
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-10-30 01:54:15 +0900

    eslint 파일 설정

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+// .eslintrc.js
+module.exports = {
+  // 개발 환경에서 사용하는 환경 정의, 브라우저, node, es6 문법 사용한다는 의미
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  extends: [
+    "eslint:recommended", 
+  ],
+}
