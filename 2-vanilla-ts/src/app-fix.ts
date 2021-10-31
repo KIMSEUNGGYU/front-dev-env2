14039a897269cdbd98620119d094c21febb7f13e
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-10-31 12:13:19 +0900

    fix typesript, lint error

--- 2-vanilla-ts/src/app-fix.ts
+++ 2-vanilla-ts/src/app-fix.ts
@@ -1,20 +1,23 @@
 // app-fix.ts
 
-function add(num1, num2) {
+function add(num1: number, num2: number) {
   return num1 + num2;
 }
-function minus(num1, num2) {
+function minus(num1: number, num2: number) {
   return num1 - num2;
 }
-function multiple(num1, num2) {
+function multiple(num1: number, num2: number) {
   return num1 * num2;
 }
-function divide(num1, num2) {
+function divide(num1: number, num2: number) {
   return num1 / num2;
 }
-function mod(num1, num2) {
+function mod(num1: number, num2: number) {
   return num1 % num2;
 }
 
 add(1, 2);
-add('1', '2');
+minus(1, 2);
+multiple(1, 2);
+divide(1, 2);
+mod(1, 2);
